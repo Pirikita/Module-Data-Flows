d999175b2fe0a7b6393950b53d5c2a0b7f9852a6
Pirikita <beatriz.oliveira.work@gmail.com>
2025-12-18 11:46:29 +0100

    wrote program to show the menu as indicated in the exercise

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,24 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+
+console.log("QTY       ITEM              TOTAL");
+
+let totalCost = 0;
+
+order.forEach((item) => {
+  const {itemName, quantity, unitPricePence} = item;
+
+  const itemTotalPence = quantity * unitPricePence;
+  const itemTotalPounds = (itemTotalPence / 100).toFixed(2);
+
+  totalCost += itemTotalPence;
+
+  console.log(
+    `${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${itemTotalPounds}`
+  );
+});
+
+console.log("");
+console.log(`Total: ${(totalCost / 100).toFixed(2)}`);
