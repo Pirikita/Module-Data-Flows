9cdf8cb8b7f3659bbc61447a37f04dabf4114444
Pirikita <beatriz.oliveira.work@gmail.com>
2025-12-19 19:16:16 +0100

    fixed syntax error on function render

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -54,9 +54,9 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+  for (let n = rowsNumber - 1; n > 0; n--) {
     table.deleteRow(n);
-  });
+  };
   //insert updated row and cells
   let length = myLibrary.length;
   for (let i = 0; i < length; i++) {
