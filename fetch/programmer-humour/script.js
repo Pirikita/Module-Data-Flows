20f082d865600ca06114c927bdd3315cc3e94479
Pirikita <beatriz.oliveira.work@gmail.com>
2025-12-23 20:56:24 +0100

    final changes to css - background adaption to all devices size final fix on js for performance and lighthouse accessibility

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -1,8 +1,10 @@
+const comicContainer = document.getElementById("container");
+
 function fetchLatestComic() {
     fetch("https://xkcd.now.sh/?comic=latest")
     .then(function (response){
         if(!response.ok) {
-            throw new Error("Not Working");
+            throw new Error("Network response was not OK");
         }
         return response.json();
     })
@@ -12,7 +14,10 @@ function fetchLatestComic() {
         const img = document.createElement("img");
         img.src = data.img;
         img.alt = data.alt || data.title;
+        img.width = data.width;
+        img.height = data.height;
 
+        comicContainer.innerHTML = "";
         comicContainer.appendChild(img);
     })
     .catch(function (error){
